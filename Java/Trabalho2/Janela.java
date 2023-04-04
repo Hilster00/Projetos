0d7ea8635aca196581b305ec4e3f522041e9e4da
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2023-04-03 21:07:03 -0300

    Update Janela.java

--- Java/Trabalho2/Janela.java
+++ Java/Trabalho2/Janela.java
@@ -22,37 +22,44 @@ import javax.swing.JToolBar;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JDesktopPane;
-
-
+import java.awt.Canvas;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 public class Janela extends JFrame {
 	int opcoes[]= new int[2];
 
 	Recorte desenho = new Recorte();
 
     
-    public void showPanel1() {
+    public void showPanel00() {
     	opcoes[0]=0;
 		opcoes[1]=0;
 		mudar_cursor(1); 
 		repaint();
     }
 
-    public void showPanel2() {
+    public void showPanel01() {
     	opcoes[0]=0;
 		opcoes[1]=1;
 		mudar_cursor(1); 
 		repaint();
     }
-    public void showPanel3() {
+    public void showPanel10() {
     	opcoes[0]=1;
 		opcoes[1]=0;
+		mudar_cursor(2); 
+		repaint();
+    }
+    public void showPanel11() {
+    	opcoes[0]=1;
+		opcoes[1]=1;
 		mudar_cursor(2); 
 		repaint();
     }
 	public Janela(int altura,int largura) {
 
 		this.add(desenho);
-		showPanel2();
+		showPanel00();
 		//configurando a janela
 		this.setTitle("Algoritmo de Recorte de Retas de Cohen-Sutherland");
 	    this.setSize(600,600);
@@ -97,14 +104,14 @@ public class Janela extends JFrame {
 						@Override
 						public void mouseClicked(MouseEvent e) {
 							
-							showPanel1();
+							showPanel00();
 
 						}
 					});	
 					r2.addMouseListener(new MouseAdapter() {
 						@Override
 						public void mouseClicked(MouseEvent e) {
-							showPanel2();
+							showPanel01();
 							
 						}
 					});
@@ -124,16 +131,13 @@ public class Janela extends JFrame {
 				c1.addMouseListener(new MouseAdapter() {
 					@Override
 					public void mouseClicked(MouseEvent e) {
-						showPanel3();
+						showPanel10();
 					}
 				});	
 				c2.addMouseListener(new MouseAdapter() {
 					@Override
 					public void mouseClicked(MouseEvent e) {
-						opcoes[0]=1;
-						opcoes[1]=1;
-						mudar_cursor(0);
-						panel.repaint();
+						showPanel11();
 					}
 				});
 				
@@ -210,11 +214,14 @@ public class Janela extends JFrame {
 		
 	}
 	class Recorte extends JComponent {
-		
+		Vector<Ponto2D> pts2 = new Vector<Ponto2D>();
+        int np2 = 0;
+   
+        boolean pronto2 = false;
 		
 		Poly poly = null;
 	    int x0, y0; 
-	    boolean pronto = true;
+	    boolean pronto1 = true;
 	    Ponto2D[] pts = new Ponto2D[4];
 	    int np1=0;
 	    
@@ -241,48 +248,71 @@ public class Janela extends JFrame {
         public Recorte() {
             addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent evt) {
-            	if(opcoes[0]==opcoes[1] && opcoes[0] == 0) {
-	            		
-	                float x = fx((evt.getX())), y = fy(evt.getY());
-	                if (np == 2) np = 0;
-	                if (np == 0) {xmin = x; ymin = y;}
-	                else {
-	                    xmax = x; ymax = y;
-	                    if (xmax < xmin) {
-	                        float t = xmax;
-	                        xmax = xmin;
-	                        xmin = t;
-	                    }
-	                    if (ymax < ymin) {
-	                        float t = ymax;
-	                        ymax = ymin;
-	                        ymin = t;
-	                    }
-	                }
-	                np++;
-                }else {
-                	if(opcoes[1]==1 && opcoes[0] == 0) {
-                		int x = evt.getX(), y = evt.getY();
-                        if (pronto) {
+            	if(opcoes[0]==0) {
+            		if(opcoes[1]==0) {
+            			float x = fx((evt.getX())), y = fy(evt.getY());
+    	                if (np == 2) np = 0;
+    	                if (np == 0) {xmin = x; ymin = y;}
+    	                else {
+    	                    xmax = x; ymax = y;
+    	                    if (xmax < xmin) {
+    	                        float t = xmax;
+    	                        xmax = xmin;
+    	                        xmin = t;
+    	                    }
+    	                    if (ymax < ymin) {
+    	                        float t = ymax;
+    	                        ymax = ymin;
+    	                        ymin = t;
+    	                    }
+    	                }
+    	                np++;
+            		}else {
+            			int x = evt.getX(), y = evt.getY();
+                        if (pronto1) {
                             poly = new Poly();
                             x0 = x; y0 = y;
-                            pronto = false;
+                            pronto1 = false;
                         }
                         if (poly.size() > 0 && Math.abs(x - x0) < 3 && Math.abs(y - y0) < 3) 
-                                pronto = true;
+                                pronto1 = true;
                         else
                             poly.addVertex(new Ponto2D(fx(x), fy(y)));
-                	}else {
-                		 float x = fx(evt.getX()), y = fy(evt.getY());
-                         if (np1 == 4) np1 = 0;
-                         pts[np1++] = new Ponto2D(x,y);
-                         
-                	}
+            		}
+	                
+                }else {
+                if(opcoes[0]==1) {
+                		if(opcoes[1]==0) {
+                			float x = fx(evt.getX()), y = fy(evt.getY());
+                            if (np1 == 4) np1 = 0;
+                            pts[np1++] = new Ponto2D(x,y);
+                		}else {
+                			float x= fx(evt.getX()), y = fy(evt.getY());
+                            if(pronto2) {
+                                pts2.removeAllElements();
+                                np2 = 0;
+                                pronto2 = false;
+                            }
+                            pts2.addElement(new Ponto2D(x,y));
+                            np2++;
+                            repaint();
+                		}
+                }else {
+                		 
+                        
+                }
                 	
                 }
             	repaint();
             }
         });
+         addKeyListener(new KeyAdapter() {
+                public void keyTyped(KeyEvent evt) {
+                   evt.getKeyChar();
+                   if (np2 >= 4) pronto2 = true;
+                   repaint();
+                }
+          });
         }
     
         int clipCode(float x, float y) {
@@ -336,23 +366,23 @@ public class Janela extends JFrame {
         @Override
         public void paint (Graphics g) {
         	
-        	if(opcoes[0]==0 && opcoes[1] == 0) {
-	            initValues();
-	            if ((np ==0)||(np==1)) drawLine(g,-4,3,4,-3);
-	            clipLine(g,-4,3,4,-3,xmin,ymin,xmax,ymax);
-	            if (np == 1) {
-	                drawLine(g,fx(0),ymin,fx(maxX),ymin);
-	                drawLine(g,xmin,fy(0),xmin,fy(maxY));
-	            } else if (np == 2) {
-	                drawLine(g,xmin, ymin, xmax, ymin);
-	                drawLine(g,xmax, ymin, xmax, ymax);
-	                drawLine(g,xmax, ymax, xmin, ymax);
-	                drawLine(g,xmin, ymax, xmin, ymin);
-	                
-	            }
-	        	}else {
-	        		if(opcoes[0]==0 && opcoes[1] == 1) {
-	        		initValues();
+        	if(opcoes[0]==0) {
+        		if(opcoes[1]==0) {
+        			initValues();
+    	            if ((np ==0)||(np==1)) drawLine(g,-4,3,4,-3);
+    	            clipLine(g,-4,3,4,-3,xmin,ymin,xmax,ymax);
+    	            if (np == 1) {
+    	                drawLine(g,fx(0),ymin,fx(maxX),ymin);
+    	                drawLine(g,xmin,fy(0),xmin,fy(maxY));
+    	            } else if (np == 2) {
+    	                drawLine(g,xmin, ymin, xmax, ymin);
+    	                drawLine(g,xmax, ymin, xmax, ymax);
+    	                drawLine(g,xmax, ymax, xmin, ymax);
+    	                drawLine(g,xmin, ymax, xmin, ymin);
+    	                
+    	            }
+        		}else {
+        			initValues();
 	                float xmin = -rWidth / 3, xmax = rWidth / 3,
 	                        ymin = -rHeight /3, ymax = rHeight / 3;
 	                
@@ -366,7 +396,7 @@ public class Janela extends JFrame {
 	                int n = poly.size();
 	                if (n == 0) return;
 	                Ponto2D pi = poly.vertexAt(0);
-	                if (!pronto) {
+	                if (!pronto1) {
 	                    g.drawRect(iX(pi.x) -2, iY(pi.y) -2, 4, 4);
 	                    for (int i = 1; i< n; i++) {
 	                        Ponto2D pp = poly.vertexAt(i);
@@ -379,7 +409,11 @@ public class Janela extends JFrame {
 	                    poly.clipPoli(xmin,ymin, xmax, ymax);
 	                    drawPoly(g, poly);
 	                }
-	        		}else {
+        		}
+	            
+	        }else {
+	        if(opcoes[0]==1) {
+	        		if(opcoes[1]==0) {
 	        			initValues();
 	        	        int left = iX(-rWidth / 2), right = iX(rWidth / 2), 
 	        	                bottom = iY(-rHeight / 2), top = iY(rHeight / 2);
@@ -388,11 +422,31 @@ public class Janela extends JFrame {
 	        	            g.drawRect(iX(pts[i].x)-2, iY(pts[i].y)-2, 4, 4);
 	        	            if (i > 0) g.drawLine(iX(pts[i-1].x), iY(pts[i-1].y), iX(pts[i].x), iY(pts[i].y));
 	        	        }
-	        	        if (np1 == 4) geraCurva(g,pts[0], pts[1], pts[2], pts[3]);
+	        	        if (np1 == 4) geraCurva(g,pts[0], pts[1], pts[2], pts[3]);	
+	        		}else {
+	        			initValues();
+	        	        g.setColor(Color.blue);
+	        	        int left = iX(-rWidth / 2), right = iX(rWidth /2), 
+	        	             bottom = iY(-rHeight / 2 ), top = iY(rHeight / 2);
+	        	        g.drawRect(left, top, right - left, bottom - top);
+	        	        Ponto2D[] p = new Ponto2D[np2];
+	        	        pts2.copyInto(p);
+	        	        if(!pronto2) {
+	        	            for (int i=0; i < np2; i++) {
+	        	                g.setColor(Color.black);
+	        	                //g.drawRect(iX(p[i].x) -2, iY(p[i].y) - 2, 4, 4);
+	        	                if (i > 0) {
+	        	                	//g.drawLine(iX(p[i-1].x), iY(p[i-1].y), iX(p[i].x), iY(p[i].y));
+	    	        	            
+	        	                }
+	        	          }
+	        	        }
+	        	        if (np2 >= 4) { g.setColor(Color.red);geraCurva(g,p);}
 	        		}
+	        		
+	        }
         	}
         }
-        
         
         
         Ponto2D medio(Ponto2D p1, Ponto2D p2) {
@@ -400,7 +454,7 @@ public class Janela extends JFrame {
         }
         
         void geraCurva(Graphics g, Ponto2D p0, Ponto2D p1, Ponto2D p2, Ponto2D p3) {
-            int x0 = iX(p0.x), y0 = iY(p0.y), x3 = iX(p3.x), y3 = iY(p3.y);
+        	int x0 = iX(p0.x), y0 = iY(p0.y), x3 = iX(p3.x), y3 = iY(p3.y);
             
             if (Math.abs(x0 - x3) <= 1 && Math.abs(y0 - y3) <=1) {
                 g.drawLine(x0, y0, x3, y3);
@@ -410,7 +464,42 @@ public class Janela extends JFrame {
                 a1=medio(a,c), b1=medio(b,c), c1=medio(a1,b1);
             geraCurva(g,p0,a,a1,c1);
             geraCurva(g,c1,b1,b,p3);
+            }             
+            
         }
+        void geraCurva(Graphics g, Ponto2D[] p) {
+            int m = 50, n = p.length;
+            float xA, yA, xB, yB, xC, yC, xD, yD;
+            float a0, a1, a2, a3, b0, b1, b2, b3;
+            float x = 0, y =0, x0, y0;
+            boolean primeiro = true;
+            for (int i = 1; i < n-2; i++) {
+                xA = p[i-1].x; xB  = p[i].x; xC = p[i+1].x; xD = p[i+2].x;
+                yA = p[i-1].y; yB  = p[i].y; yC = p[i+1].y; yD = p[i+2].y;
+                
+                a3 = (-xA + 3 * (xB - xC) + xD) / 6;
+                b3 = (-yA + 3 * (yB - yC) + yD) / 6;
+                
+                a2 = (xA - 2 * xB + xC) / 2;
+                b2 = (yA - 2 * yB + yC) / 2;
+                
+                a1 = (xC - xA) / 2;
+                b1 = (yC - yA) / 2;
+                
+                a0 = (xA + 4 * xB + xC)/6;
+                b0 = (yA + 4 * yB + yC)/6;
+                
+                for (int j=0; j<=m; j++) {
+                    x0 = x;
+                    y0 = y;
+                    float t = (float) j / (float) m;
+                    x = ((a3 * t + a2) * t + a1) * t + a0;
+                    y = ((b3 * t + b2) * t + b1) * t + b0;
+                    if (primeiro) primeiro = false;
+                    else
+                        g.drawLine(iX(x0),iY(y0),iX(x),iY(y));
+                }
+            }
         }
         void limpar() {
         	this.repaint();
