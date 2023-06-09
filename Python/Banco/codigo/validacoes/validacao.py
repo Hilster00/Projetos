0447f61183f7781dc6cec297851ccf019cbf45f6
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2023-06-09 00:24:12 -0300

    Update validacao.py

--- Python/Banco/codigo/validacoes/validacao.py
+++ Python/Banco/codigo/validacoes/validacao.py
@@ -6,19 +6,19 @@ from codigo.validacoes.importar_ctrl_c import erro_ctrl
 def validacao(id):
 
     while True:
-        id_loguin=""
+        id_login=""
         
-        while id_loguin == "":
+        while id_login == "":
             os.system("cls")
-            id_loguin=erro_ctrl("Informe o seu loguin:")
-            id_loguin=id_loguin.upper()
+            id_login=erro_ctrl("Informe o seu login:")
+            id_login=id_login.upper()
 
-        if id != id_loguin:  
-            id_loguin="invalido"
+        if id != id_login:  
+            id_login="invalido"
         break
         
-    if id_loguin != "invalido":
-        importar_senha=open(f"usuarios\\{id_loguin}\\senha.txt","r")
+    if id_login != "invalido":
+        importar_senha=open(f"usuarios\{id_login}\senha.txt","r")
         senha=importar_senha.readlines()
         importar_senha.close()
         senha=senha[0]
@@ -26,16 +26,16 @@ def validacao(id):
 
         for tentativas in range (1,4):
             print(f"Tentativa {tentativas}")
-            senha_loguin=erro_ctrl("Digite a senha de acesso:")
+            senha_login=erro_ctrl("Digite a senha de acesso:")
             
-            if senha_loguin == senha:
+            if senha_login == senha:
                 break
             
-        if senha_loguin == senha:
+        if senha_login == senha:
             autenticacao=True
 
         else:
-            print("Falha no loguin")
+            print("Falha no login")
             autenticacao=False
         
         os.system("pause")
